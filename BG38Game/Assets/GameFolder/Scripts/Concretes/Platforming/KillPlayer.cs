a01c2399e2c4ce050d0c6e1c2293558f85616ec3
Mehmet Can Şensoy <can.sensoy@metu.edu.tr>
2024-07-26 17:41:11 +0300

    Trap prefabs

---
+++ BG38Game/Assets/GameFolder/Scripts/Concretes/Platforming/KillPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BG38Game
+{
+    public class KillPlayer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
